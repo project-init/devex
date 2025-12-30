6ed49cfa0e90771ce8efaea204ddfd5ca34d75f3
Derek Rushing <20804720+drush211@users.noreply.github.com>
2025-12-30 08:08:05 -0600

    INIT-261: Handle PGAccess via prompts rather than an argument to pass in. (#10)
    * INIT-261: Handle PGAccess via prompts rather than an argument to pass in.
    
    * Update pgaccess.md

--- cmd/pgaccess/main.go
+++ cmd/pgaccess/main.go
@@ -4,18 +4,34 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/manifoldco/promptui"
 	"github.com/project-init/devex/internal/pgaccess"
 )
 
 func main() {
-	if len(os.Args) != 3 {
+	if len(os.Args) != 2 {
 		usage()
 	}
 
 	pgAccessFile := os.Args[1]
-	environment := os.Args[2]
 
-	psqlCfg, err := pgaccess.LoadPGAccessEnvironmentUser(pgAccessFile, environment)
+	environments, err := pgaccess.LoadPGAccessEnvironments(pgAccessFile)
+	if err != nil {
+		panic(err)
+	}
+
+	environment, err := selectEnvironment(environments)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s chosen. Loading environment config...\n", environment)
+	environmentConfig, found := environments[environment]
+	if !found {
+		panic(fmt.Errorf("environment %s not found in file %s", environment, pgAccessFile))
+	}
+
+	psqlCfg, err := pgaccess.LoadPGAccessEnvironment(environmentConfig)
 	if err != nil {
 		panic(err)
 	}
@@ -26,7 +42,26 @@ func main() {
 	}
 }
 
+func selectEnvironment(environments map[string]pgaccess.EnvironmentConfig) (string, error) {
+	environmentNames := make([]string, 0, len(environments))
+	for environmentName := range environments {
+		environmentNames = append(environmentNames, environmentName)
+	}
+
+	argPrompt := promptui.Select{
+		Label: "Select Environment",
+		Items: environmentNames,
+	}
+
+	_, argument, err := argPrompt.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return argument, nil
+}
+
 func usage() {
-	fmt.Println("Usage: pgaccess <pgAccessFilePath> <environment>")
+	fmt.Println("Usage: pgaccess <pgAccessFilePath>")
 	os.Exit(1)
 }
