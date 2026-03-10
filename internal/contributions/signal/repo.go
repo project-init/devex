34fb1dac837a864cbdd5a966e91bb98484488ceb
David Yoon <davidyoon@Davids-MacBook-Air.local>
2026-03-09 23:58:05 -0400

    Fixed struct -> bool

--- internal/contributions/signal/repo.go
+++ internal/contributions/signal/repo.go
@@ -34,14 +34,14 @@ func sortByRepoSignal(signalMap map[string]*types.RepoSignal) []string {
 
 func getRepoGHSignal(prs []types.PR) map[string]*types.RepoSignal {
 	signalMap := make(map[string]*types.RepoSignal)
-	contributorsByRepo := make(map[string]map[string]struct{})
+	contributorsByRepo := make(map[string]map[string]bool)
 	for _, pr := range prs {
 		if _, found := signalMap[pr.Repo]; !found {
 			signalMap[pr.Repo] = &types.RepoSignal{Repo: pr.Repo}
-			contributorsByRepo[pr.Repo] = make(map[string]struct{})
+			contributorsByRepo[pr.Repo] = make(map[string]bool)
 		}
 		repoSignal := signalMap[pr.Repo]
-		contributorsByRepo[pr.Repo][pr.Author] = struct{}{}
+		contributorsByRepo[pr.Repo][pr.Author] = true
 		repoSignal.NumPRs++
 		repoSignal.WeightedPRs += math.Max((1.0-(.05*float64(pr.TimeToMerge.Hours()/24)))*authorMultiplier(nil, pr.Author), 0.0)
 		repoSignal.TotalTimeToMerge += pr.TimeToMerge
