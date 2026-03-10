5f1999d8f5913ba2ac261008fae680009daeba17
Derek Rushing <20804720+drush211@users.noreply.github.com>
2026-03-10 10:31:05 -0500

    INIT-530: Move keygen tool to sre toolbox. (#31)
    * INIT-530: Move keygen tool to sre toolbox.
    
    * Don't care if config doesn't exist, instead just default to 32.

---
+++ internal/sre/keygen/README.md
@@ -0,0 +1,21 @@
+# Keygen
+
+The keygen cmd is meant to generate a random character string that can be used as an api key.
+
+## Configuration
+
+```yaml
+keygen:
+  length: 32
+```
+
+## Usage
+
+```shell
+sre keygen --config .sre
+```
+
+#### Description
+
+The keygen cmd gets the length from the configuration file, or defaults to 32 characters. It then uses the crypto
+package to generate the secure string before eventually printing it to console to be collected and used as you see fit.
---
+++ internal/sre/keygen/cmd.go
@@ -0,0 +1,48 @@
+package keygen
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+
+	"github.com/project-init/devex/internal/sre/config"
+	"github.com/spf13/cobra"
+)
+
+func Command() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "keygen",
+		Short: "Print an API Key based on the sre configuration file.",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			length := 32
+			cfg, _ := config.GetConfig(cmd.Context())
+			if cfg != nil && cfg.Keygen.Length != nil {
+				length = *cfg.Keygen.Length
+			}
+
+			apiKey, err := generateAPIKey(length)
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Keygen API Key: %s\n", apiKey)
+			return nil
+		},
+	}
+	return cmd
+}
+
+func generateAPIKey(nBytes int) (string, error) {
+	if nBytes < 16 {
+		return "", fmt.Errorf("nBytes too small: got %d, want at least 16", nBytes)
+	}
+
+	b := make([]byte, nBytes)
+	if _, err := rand.Read(b); err != nil {
+		return "", fmt.Errorf("generate random bytes: %w", err)
+	}
+
+	// URL-safe, no "+" "/" "=", good for config files, headers, URLs, etc.
+	return base64.RawURLEncoding.EncodeToString(b), nil
+}
