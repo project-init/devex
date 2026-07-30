27ef2c49e77a4c00d1db625aa1ebee2df7ef94b3
Yelsin Sepulveda <yelsinsepulveda@gmail.com>
2026-07-30 12:23:14 -0400

    INIT-819: add actions URL to release command output
    Print a link to the GitHub Actions tab after pushing a
    release tag so the user can monitor the deployment.
    
    Signed-off-by: Yelsin Sepulveda <yelsinsepulveda@gmail.com>

--- internal/sre/release/release.go
+++ internal/sre/release/release.go
@@ -3,6 +3,7 @@ package release
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 
 	"github.com/manifoldco/promptui"
 )
@@ -34,4 +35,26 @@ func confirmRelease(v version) error {
 
 	_, err := prompt.Run()
 	return err
+}
+
+func getActionsURL() (string, error) {
+	out, err := exec.Command("git", "remote", "get-url", "origin").Output()
+	if err != nil {
+		return "", fmt.Errorf("failed to get remote URL: %w", err)
+	}
+
+	remote := strings.TrimSpace(string(out))
+	remote = strings.TrimSuffix(remote, ".git")
+
+	// SSH format: git@github.com:org/repo
+	if path, ok := strings.CutPrefix(remote, "git@github.com:"); ok {
+		return fmt.Sprintf("https://github.com/%s/actions", path), nil
+	}
+
+	// HTTPS format: https://github.com/org/repo
+	if _, ok := strings.CutPrefix(remote, "https://github.com/"); ok {
+		return remote + "/actions", nil
+	}
+
+	return "", fmt.Errorf("unrecognized remote format: %s", remote)
 }
